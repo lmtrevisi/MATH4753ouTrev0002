ff26f5593af884a86ea0747992b8f35c79e3e06d
Luis Mario Trevisi <lmtrevisi@hotmail.com>
2020-08-28 09:47:14 -0500

    changes

---
+++ R/myf.R
@@ -0,0 +1,14 @@
+#' Square Function
+#'
+#'
+#'takes vector and returns a vector of squared components
+#' @param x
+#'
+#' @return a vector of square components
+#' @export
+#'
+#' @examples
+#' x = 1:30 ; myf(x)
+myf = function(x){
+  x^2
+}
